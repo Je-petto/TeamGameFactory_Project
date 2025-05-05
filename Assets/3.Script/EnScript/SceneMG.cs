a898511d0a4f24a84328faea09385e9c77572c00
Je-petto <bejetto98@gmail.com>
2025-05-05 23:05:16 +0900

    v0.0.15] Feat : LoadingScene추가

--- Assets/3.Script/EnScript/SceneMG.cs
+++ Assets/3.Script/EnScript/SceneMG.cs
@@ -5,35 +5,31 @@ public class SceneMG : MonoBehaviour
 {
     public void TapToStart()
     {
-        Debug.Log("탭 투 스타트");
-
         SceneManager.LoadScene(1,LoadSceneMode.Single);
     }
     public void BackToStart()
     {
-        Debug.Log("백 투 스타트");
         SceneManager.LoadScene(0,LoadSceneMode.Single);
     }
-    public void BackToMenu()
+    public void BackToMainScene()
     {
-        Debug.Log("백 투 메뉴뉴");
-
+        SceneManager.LoadScene(1,LoadSceneMode.Single);
+    }
+    public void LoadingScene()
+    {
         SceneManager.LoadScene(2,LoadSceneMode.Single);
     }
     public void StartGame()
     {
-        Debug.Log("스타트 게임");
-        SceneManager.LoadScene(2,LoadSceneMode.Single);
+        SceneManager.LoadScene(3,LoadSceneMode.Single);
     }
     public void ToRankSystem()
     {
-        Debug.Log("랭킹 UI로 이동");
-        SceneManager.LoadScene(3,LoadSceneMode.Single);
+        SceneManager.LoadScene(4,LoadSceneMode.Single);
     }
     public void GoToLog()
     {
-        Debug.Log("미션로그그 UI로 이동");
-        SceneManager.LoadScene(4,LoadSceneMode.Single);
+        SceneManager.LoadScene(5,LoadSceneMode.Single);
     }
     
 }
